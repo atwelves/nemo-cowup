7df2aedd2542afb546967f77a65fee3c340a7753
Andrew Gerard Twelves <51830799+atwelves@users.noreply.github.com>
2025-08-06 17:19:54 +0300

    Extract Gulf of Finland stations

--- EXP00/get_points.py
+++ EXP00/get_points.py
@@ -1,6 +1,18 @@
 import xarray as xr
 import numpy as np
 
+#latitude  
+#longitude
+
 ds = xr.open_dataset("temp_file.nc")
-ds = ds.isel(x=600).isel(y=250)
+mod_lats = ds.nav_lat.values
+mod_lats[mod_lats<0]=np.nan
+mod_lons = ds.nav_lon.values
+mod_lons[mod_lons<0]=np.nan
+mod_lats = np.nanmean(mod_lats,1)
+mod_lons = np.nanmean(mod_lons,0)
+arg_lon = np.argmin(np.abs(mod_lons-lon))
+arg_lat = np.argmin(np.abs(mod_lats-lat))
+ds = ds.isel(x=arg_lon).isel(y=arg_lat)
+# Write to file with temporary name
 ds.to_netcdf('output.nc')
