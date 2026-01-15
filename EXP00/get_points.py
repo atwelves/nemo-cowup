bc3e8c7a3a0f509433889e479011fc876d18b6fb
Andrew Gerard Twelves <51830799+atwelves@users.noreply.github.com>
2026-01-15 11:39:55 +0200

    Update get_points.py, bug fix

--- EXP00/get_points.py
+++ EXP00/get_points.py
@@ -11,8 +11,8 @@ mod_lons = ds.nav_lon.values
 mod_lons[mod_lons<0]=np.nan
 mod_lats = np.nanmean(mod_lats,1)
 mod_lons = np.nanmean(mod_lons,0)
-arg_lon = np.argmin(np.abs(mod_lons-lon))
-arg_lat = np.argmin(np.abs(mod_lats-lat))
+arg_lon = np.nanargmin(np.abs(mod_lons-lon))
+arg_lat = np.nanargmin(np.abs(mod_lats-lat))
 ds = ds.isel(x=arg_lon).isel(y=arg_lat)
 # Write to file with temporary name
 ds.to_netcdf('output.nc')
